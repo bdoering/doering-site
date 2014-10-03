fbf30209fd9d00ac3146ded2d88226168064eaaa
Bjoern Doering <bjoern.doering@gmail.com>
2014-10-03 22:22:46 +0200

    Completed about page

---
+++ about.md
@@ -0,0 +1,41 @@
+---
+title: About
+---
+
+About
+=====
+
+Hi, I am Björn J. Döring, and this is my private blog. I work at the [DLR](http://www.dlr.de) (German Aerospace Center) in Oberpfaffenhofen near Munich, Germany. Our [institute's](http://www.dlr.de/hr/en) principal topic is [synthetic aperture radar](https://en.wikipedia.org/wiki/Synthetic_aperture_radar) (SAR), and I am involved in the calibration of systems like [TerraSAR-X](http://www.dlr.de/eo/en/desktopdefault.aspx/tabid-5725/9296_read-15979/), [TanDEM-X](http://www.dlr.de/eo/en/desktopdefault.aspx/tabid-5727/10086_read-21046/), and [Sentinel-1](http://www.esa.int/Our_Activities/Observing_the_Earth/Copernicus/Sentinel-1). I quite enjoy designing and building new hard- and software to get as much performance out of the instruments as possible. Otherwise, I enjoy running, swimming, biking, and traveling to France where my wife is from.
+
+Contact
+-------
+
+You can reach me at [bjoern.doering@gmail.com](bjoern.doering@gmail.com). 
+
+
+Publications
+------------
+
+* Björn Döring and Marco Schwerdt: [*Introducing Equivalent Radar Cross Section – A First Step Toward new Radiometric Requirement Definitions*](https://www.researchgate.net/publication/259899549_Introducing_Equivalent_Radar_Cross_Section__A_First_Step_Toward_new_Radiometric_Requirement_Definitions). European Conference on Synthetic Aperture Radar (EUSAR), June 2014, Berlin, Germany.
+
+* Björn Döring and Marco Schwerdt: [*The Radiometric Measurement Quantity for SAR Images*](https://www.researchgate.net/publication/236992377_The_Radiometric_Measurement_Quantity_for_SAR_Images). IEEE Transactions on Geoscience and Remote Sensing, vol. 51, no. 12, Dec. 2013.
+
+* Björn J. Döring, Kersten Schmidt, Matthias Jirousek, Daniel Rudolf, Jens Reimann, Sebastian Raab, John Walter Antony, and Marco Schwerdt: [*Hierarchical Bayesian Data Analysis in Radiometric SAR System Calibration: A Case Study on Transponder Calibration with RADARSAT-2 Data*](https://www.researchgate.net/publication/259082245_Hierarchical_Bayesian_Data_Analysis_in_Radiometric_SAR_System_Calibration_A_Case_Study_on_Transponder_Calibration_with_RADARSAT-2_Data). Remote Sensing, vol. 5, no. 12, 2013.
+
+* Björn J. Döring, Philipp Looser, Matthias Jirousek, and Marco Schwerdt: [*Reference Target Correction Based on Point Target SAR Simulation*](https://www.researchgate.net/publication/225023631_Reference_Target_Correction_Based_on_Point_Target_SAR_Simulation). IEEE Transactions on Geoscience and Remote Sensing, vol. 50, no. 3, 2012.
+
+For downloads and further publications, see [ResearchGate](http://www.researchgate.net/profile/Bjoern_Doering).
+
+My ORCID iD is <http://orcid.org/0000-0002-4257-1413>. Maybe it will help to work around the Björn Döring/Bjorn Doring/Bjoern Doering problem?
+
+
+Supervised Student's Work
+-------------------------
+
+* Heinrich Axt: *Vergleichende Analyse von Antennenkonzepten und Antennenentwicklung für P-Band-Kalibriertransponder*. Hochschule f\"{u}r angewandte Wissenschaften Würzburg-Schweinfurt, 2014.
+
+* Sebastian Raab: *Planung und Durchführung einer Freifeld-RCS-Messreihe zur genauen Kalibrierung von Referenzzielen*. Hochschule für angewandte Wissenschaften Würzburg-Schweinfurt, Diplomarbeit, 2013.
+
+* Giorgio Festi: *Development and Implementation of a Digital Internal Calibration Strategy for Precise SAR Reference Targets*. Universita di Trento, Master's thesis, 2012.
+
+* Philipp Looser: *HF Design eines Transponders zur Kalibrierung satellitengestützter SAR-Systeme*. Eidgenössische Technische Hochschule Zürich, Masterarbeit, 2010.
--- site.hs
+++ site.hs
@@ -1,39 +1,77 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Data.Monoid (mappend, (<>))
+import           Text.Pandoc 
+import           Control.Monad (forM_)
 import           Hakyll
+import qualified Data.Map as M
+    
+--------------------------------------------------------------------
+-- Contexts
+--------------------------------------------------------------------
 
---------------------------------------------------------------------------------
-main :: IO ()
-main = hakyll $ do
-  match (fromList ["assets/images/*", "assets/ico/*"]) $ do
-    route   idRoute
-    compile copyFileCompiler
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y"
+  <> mathCtx
+  <> defaultContext
 
+mathCtx :: Context String
+mathCtx = field "mathjax" $ \item -> do
+  metadata <- getMetadata $ itemIdentifier item
+  return $ if "mathjax" `M.member` metadata
+           then "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>"
+           else ""
+
+archiveCtx posts =
+  listField "posts" postCtx (return posts)
+  <> constField "title" "Archives"
+  <> defaultContext
+
+indexCtx posts =
+  listField "posts" postCtx (return posts)
+    <> constField "title" "Home"
+    <> defaultContext
+
+--------------------------------------------------------------------
+-- Rules
+--------------------------------------------------------------------
+
+static :: Rules ()
+static = do
+  let sources = [ "assets/images/*"
+                , "assets/ico/*"
+                , "assets/js/*"
+                , "assets/fonts/*" ]
+  forM_ sources $ \src -> 
+    match src $ do
+      route   idRoute
+      compile copyFileCompiler
+
+index :: Rules ()
+index = do
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let idxCtx = indexCtx posts
+      getResourceBody
+        >>= applyAsTemplate idxCtx
+        >>= loadAndApplyTemplate "templates/default.html" idxCtx
+        >>= relativizeUrls 
+
+less :: Rules ()
+less = do
   -- No matter which less file changes, always recompile the same file
   match "less/*.less" $ do
     route . constRoute $ "assets/css/bdoering.css"
     compile $ getResourceString >>=
-  --    withItemBody (unixFilter "lessc" ["--paths", "./less", "-"]) >>=
       withItemBody (unixFilter "lessc" ["less/bdoering.less"]) >>=
       return . fmap compressCss
       >>= relativizeUrls
 
-  match "assets/js/*" $ do
-    route idRoute
-    compile copyFileCompiler
-
-  match "assets/fonts/*" $ do
-    route idRoute
-    compile copyFileCompiler
-
-
-  match (fromList ["about.rst", "contact.markdown"]) $ do
-    route   $ setExtension "html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/default.html" defaultContext
-      >>= relativizeUrls
-
+posts :: Rules ()
+posts = do
   match "posts/*" $ do
     route $ setExtension "html"
     compile $ pandocCompiler
@@ -41,37 +79,59 @@ main = hakyll $ do
       >>= loadAndApplyTemplate "templates/default.html" postCtx
       >>= relativizeUrls
 
+archive :: Rules ()
+archive = do
   create ["archive.html"] $ do
     route idRoute
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
-      let archiveCtx =
-            listField "posts" postCtx (return posts) `mappend`
-            constField "title" "Archives"            `mappend`
-            defaultContext
+      let archiveCtx' = archiveCtx posts
       makeItem ""
-        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx'
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx'
         >>= relativizeUrls
 
-
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let indexCtx =
-            listField "posts" postCtx (return posts) `mappend`
-            constField "title" "Home"                `mappend`
-            defaultContext
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" indexCtx
-        >>= relativizeUrls
-
+templates :: Rules ()
+templates = do
   match "templates/*" $ compile templateCompiler
 
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-  dateField "date" "%B %e, %Y" `mappend`
-  defaultContext
+otherPages :: Rules ()
+otherPages = do
+  let src = ["about.md"]
+  match (fromList src) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
+
+--------------------------------------------------------------------
+-- Configuration
+--------------------------------------------------------------------
+
+compiler :: Compiler (Item String)
+compiler = pandocCompilerWith defaultHakyllReaderOptions pandocOptions
+
+pandocOptions :: WriterOptions
+pandocOptions = defaultHakyllWriterOptions{ writerHTMLMathMethod = MathJax "" }
+
+cfg :: Configuration
+cfg = defaultConfiguration
+
+--------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------
+      
+main :: IO ()
+main = hakyllWith cfg $ do
+  index
+  static
+  less
+  posts
+  archive
+  templates
+  otherPages
+  
+
+
+
+
